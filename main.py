6695a2c0e0160f7321b55a9c9bc01fbbfa69adcc
Ali Ameur <aliameur169@gmail.com>
2023-03-11 15:47:27 +0000

    major restructure

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import smtplib
 import os
 from dotenv import load_dotenv
 from flask_talisman import Talisman
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Email
+import uuid
 
 load_dotenv()
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+app.config['SECRET_KEY'] = uuid.uuid4().hex
+csrf = CSRFProtect(app)
+
 
 csp = {
     'default-src': [
@@ -27,8 +35,34 @@ csp = {
 talisman = Talisman(app, content_security_policy=csp)
 
 
+class ContactForm(FlaskForm):
+    name = StringField('My name is', [DataRequired()], render_kw={'placeholder': 'Full Name'})
+    email = StringField('Full Name', [Email()], render_kw={'placeholder': 'name@example.com'})
+    message = TextAreaField('Your message', [DataRequired()], render_kw={'placeholder': 'I want to say that...'})
+
+
+def email_admin(name, email, message):
+    with smtplib.SMTP("smtp.gmail.com", 587, timeout=120) as connection:
+        connection.starttls()
+        server_email = os.getenv('EMAIL')
+        server_pass = os.getenv('PASSWORD')
+        admin_email = os.getenv('ADMIN_EMAIL')
+        connection.login(server_email, server_pass)
+        connection.sendmail(from_addr=server_email,
+                            to_addrs=admin_email,
+                            msg="Subject: New Message From Business Website\n\n"
+                                f"Name: {name}\n"
+                                f"Email: {email}\n"
+                                f"Message: {message}\n")
+
+
 @app.route('/', methods=["POST", "GET"])
 def home():
+    form = ContactForm()
+    for i in form:
+        print(i)
+    if form.validate_on_submit():
+        email_admin(name=form.name, email=form.email, message=form.message)
     if request.method == "POST":
         data = request.form
         with smtplib.SMTP("smtp.gmail.com", 587, timeout=120) as connection:
@@ -43,7 +77,7 @@ def home():
                                     f"Name: {data['name']}\n"
                                     f"Email: {data['email']}\n"
                                     f"Message: {data['message']}\n")
-    return render_template("home.html")
+    return render_template("home.html", form=form)
 
 
 if __name__ == '__main__':
