60e32deba15547bbe1726a01ffe8cd84e8a16a5e
Ali <aliameur169@gmail.com>
2023-03-22 17:51:15 +0000

    add sitemap

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from flask_talisman import Talisman
 from flask_wtf.csrf import CSRFProtect
 import uuid
 from forms import ContactForm
+from flask_sitemap import Sitemap
 
 load_dotenv()
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 app.config['SECRET_KEY'] = uuid.uuid4().hex
 csrf = CSRFProtect(app)
+sitemap = Sitemap(app)
 
 
 csp = {
@@ -54,21 +56,12 @@ def home():
     form = ContactForm()
     if form.validate_on_submit():
         email_admin(name=form.name, email=form.email, message=form.message)
-    if request.method == "POST":
-        data = request.form
-        with smtplib.SMTP("smtp.gmail.com", 587, timeout=120) as connection:
-            connection.starttls()
-            server_email = os.getenv('EMAIL')
-            server_pass = os.getenv('PASSWORD')
-            admin_email = os.getenv('ADMIN_EMAIL')
-            connection.login(server_email, server_pass)
-            connection.sendmail(from_addr=server_email,
-                                to_addrs=admin_email,
-                                msg="Subject: New Message From Business Website\n\n"
-                                    f"Name: {data['name']}\n"
-                                    f"Email: {data['email']}\n"
-                                    f"Message: {data['message']}\n")
     return render_template("home.html", form=form)
+
+
+@sitemap.register_generator
+def home():
+    yield 'home', {}
 
 
 if __name__ == '__main__':
